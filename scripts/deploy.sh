21521a7ad74d58f4c66ca915aa58902174ed1ea3
ilusha2274 <ilya.Shamanin@yandex.ru>
2022-02-11 17:33:13 +0300

    test deploy2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,11 +9,7 @@ scp target/Messenger3.0-0.0.1-SNAPSHOT.jar\
 
 echo 'Restart server...'
 
-ssh root@178.208.92.181 << EOF
+ssh root@178.208.92.181 "pgrep java | xargs kill -9; nohup java -jar ~/messenger3/Messenger3.0-0.0.1-SNAPSHOT.jar > ~/messenger3/log.txt 2>&1 &"
 
-pgrep java | xargs kill -9
-nohup java -jar ~/messenger3/Messenger3.0-0.0.1-SNAPSHOT.jar > ~/messenger3/log.txt &
-
-EOF
 
 echo 'Bye'
